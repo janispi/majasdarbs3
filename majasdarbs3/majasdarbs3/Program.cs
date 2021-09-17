3d4a04f9a412eb2f5aba87b9c7edb406b7f8c5a4
janispi <janis.pirsko@gmail.com>
2021-09-17 13:58:50 +0300

    1uzd

---
+++ majasdarbs3/majasdarbs3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace majasdarbs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner("Jānis");
+            
+
+            owner.Phone = "Jānis";
+            Console.WriteLine("Kāda ir Jūsu telefona marka?");
+            owner.Brand = Console.ReadLine();
+            Console.WriteLine("Kāds ir Jūsu telefona modelis?");
+            owner.Model = Console.ReadLine();
+            Console.WriteLine("Ievadiet telefona garumu, platumu un augstumu centimetros");
+            owner.Size = Console.ReadLine();
+            
+
+
+            string greeting = owner.GetGreeting();
+
+            Console.WriteLine(greeting);
+        }
+    }
+}
+//-------------------------------------------------------------------
---
+++ majasdarbs3/majasdarbs3/telefons.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace majasdarbs3
+{
+    public class Owner
+    {
+
+        public Owner(string name)
+        {
+            Phone = name;
+        }
+
+        public string Phone { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Size { get; set; }
+
+
+        public string GetGreeting()
+        {
+            string greeting = "Hello, you have " + Brand + Model + ", congrats";
+
+            return greeting;
+        }
+        public void Call()
+        {
+            Console.WriteLine("calling");
+        }
+        public void SendMessage()
+        {
+            Console.WriteLine("sending message");
+        }
+
+    }
+}
+//----------------------------------------------------
